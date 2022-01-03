01e16b6f1f5e3bad802198e2592cbc118f71dcee
Andrea Schack <andr45f9@stud.kea.dk>
2022-01-03 17:51:36 +0100

    animation + burgermenu

---
+++ script.js
@@ -0,0 +1,10 @@
+/* Burger menu */
+
+function myFunction() {
+  const x = document.getElementById("navLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
